c71d928b6be42ec70a7cc04d71e733ebd4138470
wcd923 <wernerdiers@gmail.com>
2017-06-08 11:56:24 -0300

    comment here

--- API/api-jenkins.groovy
+++ API/api-jenkins.groovy
@@ -3,62 +3,46 @@
 import groovy.json.JsonSlurper
 
 node() {
-    def any_success = false
-    BUILD_JOB_NAME = "api-jenkins-build-job"
+    BUILD_JOB_NAME = "api-jenkins-build-job-parameters"
     JENKINS_IP_PORT = "10.1.2.10:8080"
     CREDENTIALS = "admin"
 
-    stage('check'){
-        jobStatus = getJobStatus("${BUILD_JOB_NAME}")
-        print "Job status: $jobStatus"
-
-        if(jobStatus == "SUCCESS" || jobStatus == "UNSTABLE" || jobStatus == 201 || jobStatus == 200){
-            any_success = true
-        }else{
-            echo "${${BUILD_JOB_NAME}} FAILED"
-        }
+    stage('BUILD-JOB'){
+        buildJobCrumbWithParameter("${BUILD_JOB_NAME}")
     }
 
-    stage('post'){
-        //buildJobCrumb("${BUILD_JOB_NAME}")
-        buildJobCrumbWithParameter("${BUILD_JOB_NAME}")
+    stage('GET-STATUS'){
+        jobStatus = getLastJobStatus("${BUILD_JOB_NAME}")
+        print "Job status: $jobStatus"
+
     }
 }
 
 
 def getCrumbIssuer(){
-    def rx = httpRequest url: "${env.JENKINS_URL}crumbIssuer/api/json", authentication: "${CREDENTIALS}"
-    println('Status: '+ rx.status )
-    println('Response: '+ rx.content)
+    def rx = httpRequest httpMode: 'GET',\
+            url: "${env.JENKINS_URL}crumbIssuer/api/json",\
+            authentication: "${CREDENTIALS}"
+    //println('Status: '+ rx.status )
+    //println('Response: '+ rx.content)
     def rxJson = new JsonSlurper().parseText(rx.getContent())
     return rxJson['crumb']
 }
 
 
-def getJobStatus(String jobName) {
-    def rx = httpRequest url: "${env.JENKINS_URL}job/${BUILD_JOB_NAME}/lastBuild/api/json",\
-            authentication: "${CREDENTIALS}"
+def getLastJobStatus(String jobName) {
+    def rx = httpRequest httpMode: 'GET',\
+            url: "${env.JENKINS_URL}job/${BUILD_JOB_NAME}/lastBuild/api/json?pretty=true",\
+            authentication: "${CREDENTIALS}",\
+            ignoreSslErrors: true,\
+            validResponseCodes: '100:500',\
+            acceptType: 'APPLICATION_JSON',\
+            contentType: 'APPLICATION_JSON',\
+            consoleLogResponseBody: true
 
-    println('Status: ' + rx.status)
-    println('Response: ' + rx.content)
-
+    println('***Response: ' + rx.content)
     def rxJson = new JsonSlurper().parseText(rx.getContent())
     return rxJson['result']
-}
-
-
-def buildJob(String jobName){
-    def rx = httpRequest url: "${env.JENKINS_URL}job/${env.JOB_NAME}/build/api/json",\
-            authentication: "${CREDENTIALS}",\
-            httpMode: 'POST',\
-            acceptType: 'APPLICATION_JSON',\
-            contentType: 'APPLICATION_JSON'
-
-    println('Status: '+ rx.status)
-    println('Response: '+ rx.content)
-    println('All: '+ rx)
-    //def rxJson = new JsonSlurper().parseText(rx.getContent())
-    return rx.status
 }
 
 
@@ -85,41 +69,20 @@ def buildJobCrumbWithParameter(String jobName){
     def PARAM_ONE = "Ruben_pagate_las_facturas"
     def PARAM_TWO = "Ruben_botooooon"
     def CRUMB = getCrumbIssuer()
+    def URL_JOB = ["${env.JENKINS_URL}job/${BUILD_JOB_NAME}/buildWithParameters/api/json?",
+                   "PARAM_ONE=${PARAM_ONE}&",
+                   "PARAM_TWO=${PARAM_TWO}"
+    ]
 
-    def rx = httpRequest customHeaders: [[name:'Jenkins-Crumb',\
-            value: "${CRUMB}"]],\
-            httpMode: 'POST',\
+    def rx = httpRequest httpMode: 'POST',\
+            customHeaders: [[name:'Jenkins-Crumb',value: "${CRUMB}"]],\
             ignoreSslErrors: true,\
             responseHandle: 'LEAVE_OPEN',\
-            url: "${env.JENKINS_URL}job/${BUILD_JOB_NAME}/build?PARAM_ONE=${PARAM_ONE}&PARAM_TWO=${PARAM_TWO}",\
+            url: URL_JOB.join(""),\
             authentication: "${CREDENTIALS}",\
-            validResponseCodes: '100:500'
-
-    println('Status: '+ rx.status)
-    println('Response: '+ rx.content)
-    println('All: '+ rx)
-    //def rxJson = new JsonSlurper().parseText(rx.getContent())
-    return rx.status
-}
-
-
-def buildJobWithParameter(String jobName){
-
-    def messageBody = ['app_name': openshift, 'project': devops ]
-
-    def rx = httpRequest url: "${env.JENKINS_URL}job/job-parameter/buildWithParameters",\
-            authentication: "${CREDENTIALS}",\
-            httpMode: 'POST',\
+            validResponseCodes: '100:500',\
             acceptType: 'APPLICATION_JSON',\
-            contentType: 'APPLICATION_JSON', \
-            requestBody: messageBody
-
-    println('Status: '+ rx.status)
-    println('Response: '+ rx.content)
-    println('All: '+ rx)
-    //def rxJson = new JsonSlurper().parseText(rx.getContent())
+            contentType: 'APPLICATION_JSON',\
+            outputFile: 'response_file.json'
     return rx.status
 }
-
-
-PROBAR CON POST http://10.1.2.10:8080/job/api-jenkins-build-job/build?PARAM_ONE=Ruben_pagate_las_facturas&PARAM_TWO=Ruben_botooooon
